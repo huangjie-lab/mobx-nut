e55fe4b4046e2c88bb41745ca408c6217e067605
huangjie <huangjie6@codemao.cn>
2025-02-22 22:13:17 +0800

    fix: 两个订阅方法让组件重新渲染

--- src/pages/CountPage.tsx
+++ src/pages/CountPage.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useReducer } from "react";
+import { useEffect, useReducer, useState } from "react";
 import count from "../store/Count";
 // import { observer } from "mobx-react";
 import { observer } from "../whtch";
@@ -6,8 +6,10 @@ import { observer } from "../whtch";
 function CountPage() {
   // 手动订阅刷新数据 redux同理
   // const [, forceUpdate] = useReducer((x) => x + 1, 0);
+  // const [, setState] = useState();
   // useEffect(() => {
   //   count.subscribe(forceUpdate);
+  //   const.subscribe(() => setState([] as any))
   // }, []);
   return (
     <div>
